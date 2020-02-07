bde6eb8ec63102816e0647ad5bed030633a45e30
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-07 11:32:30 -0500

    Added if statements to function handleCheck

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,21 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting to check inbetween!')
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+
     lastChecked = this;
 }
 
